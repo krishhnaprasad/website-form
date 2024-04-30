8c1049127266240bbf604ee16f8d99df82d60da4
krishhnaprasad <krishnaprasadp308@gmail.com>
2024-04-30 13:43:34 +0530

    added fields on 3 models

--- odoo-17.0/custom_addons/al_arab/models/tax_invoice.py
+++ odoo-17.0/custom_addons/al_arab/models/tax_invoice.py
@@ -16,4 +16,12 @@ class ResCompany(models.Model):
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    delivery_charge = fields.Monetary(string="Delivery Charge")
+    delivery_charge = fields.Monetary(string='Delivery Charge', compute='_compute_delivery_charge', store=True)
+
+    @api.depends('partner_id')
+    def _compute_delivery_charge(self):
+        for move in self:
+            if move.partner_id.area == 'Local':
+                move.delivery_charge = 5.00
+            else:
+                move.delivery_charge = 10.00
